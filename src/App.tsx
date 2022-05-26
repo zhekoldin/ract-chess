b171d88f43a371f4557538712bf1e00afe5dbc7d
Roman Zhekoldin <zhekoldin@bk.ru>
2022-05-26 13:09:20 +0500

    start working

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import "./App.css";
+import BoardComponent from './components/BoardComponent';
 
 const App = () => {
   return (
-    <div>
-
+    <div className='app'>
+        <BoardComponent/>
     </div>
   );
 };
---
+++ src/components/CellComponent.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CellComponent = () => {
+    return (
+        <div
+        className="cell">
+            
+        </div>
+    )
+}
+
+export default CellComponent;
